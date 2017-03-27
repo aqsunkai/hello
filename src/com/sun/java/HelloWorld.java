ca0eff3d985ade3f66b8d7f06c973783b0a663ac
sunkai <aqsunkai@163.com>
2017-03-27 21:04:07 +0800

    first commit

---
+++ src/com/sun/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sun.java;
+
+/**
+ * Created by sun on 2017-3-27.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
